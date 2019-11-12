f0471a35ed5a6abb78d417eabe42c2b917ac95cd
Arthur-Gantier <arthur.gantier@gmail.com>
2019-11-12 11:54:58 +0100

    Quelques ajouts
    Ajouts de vrais textes quasi-définitifs, un header animé, une accroche, refonte de la grid.

--- java_index.js
+++ java_index.js
@@ -3,10 +3,12 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
-    document.getElementById("header").style.backgroundColor = "rgba(247, 247, 247, 0.3)";
+    document.getElementById("header").style.fontSize = "10px";
+    document.getElementById("header").style.backgroundColor = "rgba(247, 247, 247, .5)";
+    document.getElementById("logo").style.width = "5%";
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("header").style.fontSize = "30px";
     document.getElementById("header").style.backgroundColor = "rgba(247, 247, 247, 1)";
+    document.getElementById("logo").style.width = "10%";
   }
 }
