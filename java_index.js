7c7f8431211477fed229e5c18b0f4c1e3266c85c
Arthur-Gantier <arthur.gantier@gmail.com>
2019-11-18 16:48:59 +0100

    Grid/placeholder etc..
    Ajouts de place holder pour la section diplome et expérience. Supression du logo en en-tête. Modification de certains div en section pour plus de clarté dans le texte

--- java_index.js
+++ java_index.js
@@ -5,10 +5,8 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.fontSize = "10px";
     document.getElementById("header").style.backgroundColor = "rgba(247, 247, 247, .5)";
-    document.getElementById("logo").style.width = "5%";
   } else {
     document.getElementById("header").style.fontSize = "30px";
     document.getElementById("header").style.backgroundColor = "rgba(247, 247, 247, 1)";
-    document.getElementById("logo").style.width = "10%";
   }
 }
